e9b00faf161771a0d135a782052a4ded6b589c66
Soham Kaje <kaje.soham@gmail.com>
2024-03-12 15:22:15 -0500

    Update riemann.py
    added descriptions for the functions

--- riemann.py
+++ riemann.py
@@ -1,13 +1,36 @@
+# imports
 import matplotlib.pyplot as PLT
 import matplotlib as mpl
 import numpy as np
 import menus
 
+#--------------------------------------------#
+# There are many approaches to these functions,
+# feel free to code what you think works best
+#--------------------------------------------#
+
+#
+# leftSum
+#
+# Creates lists for left sum 
+# 
 def leftSum():
     pass
 
+
+#
+# rightSum
+#
+# Creates lists for right sum
+#
 def rightSum():
     pass
 
+
+#
+# midpointSum
+#
+# Creates list for midpoint sum
+#
 def midpointSum():
     pass
