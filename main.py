303babe9f30e86eb24df2f9d0f77ff299b7b4d55
davidsavvy <davesavenok@gmail.com>
2024-03-12 15:23:46 -0500

    more start updates

--- main.py
+++ main.py
@@ -4,8 +4,25 @@ import numpy as np
 import menus
 import riemann
 
-def outputMenu ():
-    print("needs work")
+def menuOutputManager():
+    pass
+
+def inputManager():
+    pass
+
+def evaluationManager():
+    # include outputs
+    pass
+
+def buildGraph():
+    pass
+
+# STEPS:
+# 1. Display program information and usage
+# 2. Gather user input on function (wolfram syntax), bounds, dx, etc.
+# 3. Evaluate selected sum (left, right, midpoint)
+# 4. Output to terminal through outputInfo() in menus.py
+# 5. Go back and use matplotlib plots and bars to make a graph 
 
 if __name__ == "__main__":
     outputMenu()
